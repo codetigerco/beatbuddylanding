c23f53fb691947ebe3e78d37660e1d3606151a2a
Vivian Guillen <vivian@viviangb.com>
2016-09-19 21:46:36 -0400

    First draft of cumbre site

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+
+// Load plugins
+var gulp = require('gulp'),
+      browserSync = require('browser-sync');
+
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init(["_site/css/*.css", "_site/js/*.js", '_site/*.html'], {
+        proxy:  "localhost:4000"
+    });
+});
+
+// Watch
+gulp.task('watch', ['browser-sync'], function() {
+
+  gulp.watch('_site/*.html');
+
+});
